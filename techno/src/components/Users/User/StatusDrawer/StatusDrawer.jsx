7a3710b57aa23b6f269ce8728e38a915f9fc1f3b
Nikita <treskov-1@mail.ru>
2021-05-03 10:28:13 +0300

    inline css gone to module

--- techno/src/components/Users/User/StatusDrawer/StatusDrawer.jsx
+++ techno/src/components/Users/User/StatusDrawer/StatusDrawer.jsx
@@ -1,8 +1,7 @@
 import styles from './StatusDrawer.module.css'
 
 export default function StatusDrawer(props) {
-    const statusCode = props.status;
-    console.log(props);
+    /*const statusCode = props.status;
     const styleBlock = {
         display: 'block',
         position: 'absolute',
@@ -15,17 +14,15 @@ export default function StatusDrawer(props) {
         borderRadius: '6px',
         padding: '12px 16px',
         zIndex: 9999
-    }
+    }*/
 
-    const window = (
-        <div style={styleBlock}>
-            <div className={styles.statusChosen} style={{color: statusCode === 0 && '#3F3356'}}>Приостановлена</div>
-            <div className={styles.statusChosen} style={{color: statusCode === 1 && '#3F3356'}}>Подписка активна</div>
-            <div className={styles.statusChosen} style={{color: statusCode === 2 && '#3F3356'}}>Заблокирован</div>
-        </div>
-    )
+    const {status, coordX, coordY} = props;
 
-    return (
-        window
-    )
+    const window = <div className={styles.statusWindow} style={{left: coordX.toString() + 'px', top: (coordY + 22).toString() + 'px'}}>
+        <div className={styles.statusChosen} style={{color: status === 0 && '#3F3356'}}>Приостановлена</div>
+        <div className={styles.statusChosen} style={{color: status === 1 && '#3F3356'}}>Подписка активна</div>
+        <div className={styles.statusChosen} style={{color: status === 2 && '#3F3356'}}>Заблокирован</div>
+    </div>
+
+    return window
 }
